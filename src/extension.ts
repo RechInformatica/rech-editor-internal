93e3b7052ca16c48a70e226b4e9a74365265e249
Cassel <cassel@rech.com.br>
2018-09-11 14:35:01 -0300

    Open dialog extensions transferred from rech-editor-vscode to rech-editor-internal

--- src/extension.ts
+++ src/extension.ts
@@ -7,23 +7,24 @@ import { Editor } from 'rech-editor-vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "rech-editor-internal" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    context.subscriptions.push(vscode.commands.registerCommand('extension.openFontesTrunk', () => {
+        // TODO: this command must be transferred to rech-editor-internal
         // The code you place here will be executed every time your command is executed
-        let editor = new Editor();
-        console.log(editor.getPath());
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+        var editor = new Editor();
+        editor.showOpenDialog(
+            'F:\\FONTES\\',
+            (currentFile) => { editor.openFile(currentFile) },
+        );
+    }));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.openScripts', () => {
+        // TODO: this command must be transferred to rech-editor-internal
+        // The code you place here will be executed every time your command is executed
+        var editor = new Editor();
+        editor.showOpenDialog(
+            'F:\\BAT\\',
+            (currentFile) => { editor.openFile(currentFile) },
+        );
+    }));
 }
 
 // this method is called when your extension is deactivated
