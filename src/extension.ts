89d83b6249c645df345db3ae3c313379b3d537c6
Cassel <cassel@rech.com.br>
2018-09-13 14:47:53 -0300

    Created command for opening dialog in user's Working-Copy

--- src/extension.ts
+++ src/extension.ts
@@ -2,29 +2,35 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { Editor } from 'rech-editor-vscode';
+import Editor      from 'rech-editor-vscode';
+import WorkingCopy from './wc/WorkingCopy';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand('extension.openFontesTrunk', () => {
-        // TODO: this command must be transferred to rech-editor-internal
-        // The code you place here will be executed every time your command is executed
-        var editor = new Editor();
-        editor.showOpenDialog(
-            'F:\\FONTES\\',
-            (currentFile) => { editor.openFile(currentFile) },
-        );
+        showOpenDialog('F:\\FONTES\\');
     }));
     context.subscriptions.push(vscode.commands.registerCommand('extension.openScripts', () => {
-        // TODO: this command must be transferred to rech-editor-internal
-        // The code you place here will be executed every time your command is executed
-        var editor = new Editor();
-        editor.showOpenDialog(
-            'F:\\BAT\\',
-            (currentFile) => { editor.openFile(currentFile) },
-        );
+        showOpenDialog('F:\\BAT\\');
     }));
+    context.subscriptions.push(vscode.commands.registerCommand('extension.openWc', () => {
+        showOpenDialog(WorkingCopy.current().getSourcesDir());
+    }));
+
+}
+
+/**
+ * Opens dialog for file selection and automatically opens the files in editor
+ * 
+ * @param defaultDir default directory
+ */
+function showOpenDialog(defaultDir: string) {
+    var editor = new Editor();
+    editor.showOpenDialog(
+        defaultDir,
+       (currentFile) => { editor.openFile(currentFile) },
+   );
 }
 
 // this method is called when your extension is deactivated
---
+++ src/wc/WorkingCopy.ts
@@ -0,0 +1,53 @@
+/**
+ * Class for returning Working-Copy general information
+ */
+export default class WorkingCopy {
+
+    /** Name of the working-copy */
+    name: string;
+    /** Version of the working-copy */
+    version: string;
+
+    /**
+     * Creates an instance of Working-Copy class with the specified WC name
+     * 
+     * @param wc Working-Copy name
+     */
+    constructor(name: string, version: string) {
+        this.name = name;
+        this.version = version;
+    }
+
+    /**
+     * Returns an instance of the current WorkingCopy
+     */
+    public static current() {
+        var parts = WorkingCopy.getWorkingCopyString().split(" ");
+        var currentName = "";
+        var currentVersion = "";
+        if (parts) {
+            if (parts.length > 0) {
+                currentName = parts[0];
+            }
+            if (parts.length > 1) {
+                currentVersion = parts[1];
+            }
+        }
+        return new WorkingCopy(currentName, currentVersion);
+    }
+
+    /**
+     * Returns a String representation of the current user's Working Copy
+     */
+    private static getWorkingCopyString() {
+        return require("os").userInfo().username + " des";
+    }
+
+    /**
+     * Returns the directory where Cobol files are located
+     */
+    public getSourcesDir() {
+        return "F:\\SIGER\\wc\\" + this.version + "\\" + this.name + "\\fon\\";
+    }
+
+}
