9912d5b14bb3e717364272a725051c93902f5b88
Vinicius <vinicius@rech.com.br>
2018-10-31 16:46:45 -0300

    Improved the OpenWFPF.ts file

--- src/open/OpenWFPF.ts
+++ src/open/OpenWFPF.ts
@@ -5,24 +5,31 @@ import { Editor, File } from "rech-editor-vscode";
  */
 export class OpenWFPF {
 
-    /**
-     * 
-     */
+    /** Editor object */
     public editor: Editor;
+
+    /**
+     * Constructor of OpenWFPF
+     */
     constructor () {
         this.editor = new Editor();
     }
 
     /**
-     * 
+     * Opens the WFPF files from the CBL file or opens the CBL file from the WF or PF file
      */
     public open() {
+        /** RegEx to find WFPF files */
+        const regexWFPF = /(.*)(WF|PF).CPY/gi;
+        /** RegEx to find .CBL files */
+        const regexCBL = /(.*).CBL/gi;
+        
         let filename = this.editor.getCurrentFileName();
-        let result = /(.*)(WF|PF).CPY/gi.exec(filename);
+        let result = regexWFPF.exec(filename);
         if (result) {
             this.editor.openFile(result[1] + ".CBL");
         } else {
-            result = /(.*).CBL/gi.exec(filename);
+            result = regexCBL.exec(filename);
             if (result) {
                 this.editor.openFile(result[1] + "WF.CPY");
                 this.editor.openFile(result[1] + "PF.CPY");
