4ce0cedb5f15b6e80caa169fbc287dd16d0af0de
Vinicius <vinicius@rech.com.br>
2018-11-08 17:41:39 -0200

    Added new function to the shift + alt + F11 shortcut to position the cursor over the respective function when used on a WF | PF file

--- src/open/OpenWFPF.ts
+++ src/open/OpenWFPF.ts
@@ -1,4 +1,4 @@
-import { Editor, File } from "rech-editor-vscode";
+import { Editor, File, Scan} from "rech-editor-vscode";
 
 /**
  * Class for opening wf/pf files
@@ -23,11 +23,33 @@ export class OpenWFPF {
         const regexWFPF = /(.*)(WF|PF).CPY/gi;
         /** RegEx to find .CBL files */
         const regexCBL = /(.*).CBL/gi;
+        /** RegEx to find the constant from the function where the cursor is positioned */
+        const regexFunction = /\s([A-Z]+\-FUN\-[A-Z]+)/gi;
         
+        /** Get the current file name */
         let filename = this.editor.getCurrentFileName();
+        /** If the current file is a WF | PF file */
         let result = regexWFPF.exec(filename);
         if (result) {
-            this.editor.openFile(result[1] + ".CBL");
+            /** Name of the Cobol extension file */
+            let cobolFile = result[1] + ".CBL";
+            this.editor.openFile(cobolFile, () => {
+                /** Name of the function */
+                let resultFunction = regexFunction.exec(this.editor.getCurrentLine());
+                /** If the cursor is over the name of a function */
+                if (resultFunction) {
+                    new File(cobolFile).loadBuffer().then((buffer) => {
+                        /** Search for the name of the function in the Cobol File and then position it on the respective line */
+                        new Scan(buffer.toString()).scan(new RegExp((<string[]>resultFunction)[1], 'gi'), (iterator: any) => {
+                            new Editor().openFile(cobolFile, () => {
+                                new Editor().setCursor(iterator.row, iterator.column);
+                            });
+                            iterator.stop();
+                        });
+                    });
+                }
+            });
+        /** If the current file is a Cobol file */
         } else {
             result = regexCBL.exec(filename);
             if (result) {
