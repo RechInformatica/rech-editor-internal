56e0a46554e5123a1d4d4c1d4e08fd0ae46f1a2d
Vanduir <vanduir@rech.com.br>
2018-11-12 15:17:54 -0200

    Call the autogrep function to find the CBL source to preprocess

---
+++ src/autogrep/autogrep.ts
@@ -0,0 +1,94 @@
+import { Executor, Process, File, Path } from 'rech-editor-vscode';
+
+ /**
+ * Class to find use of copys
+ */
+export class Autogrep {
+
+  /** List of sources to find */
+  private sources: string[] | undefined;
+
+  public constructor(sources?: string[]){
+    this.sources = sources;
+  }
+
+  /**
+   * Find uses of sources
+   */
+  public find(): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+      this.callAutogrep().then((process: Process) => {
+        let cblFiles = process.getStdout().match(/.*\.CBL /gi);
+        if (cblFiles) {
+          resolve(this.sortResultsAccordingSourceName(cblFiles));
+        } else {
+          reject();
+        }
+      });
+    });
+  }
+
+  /**
+   * Call the autogrep
+   */
+  private callAutogrep(): Promise<Process> {
+    return new Promise((resolve) => {
+      let commandline = this.buildCommandLine();
+      if (commandline) {
+        new Executor().runAsync(commandline, (process: Process) => {
+          resolve(process);
+        });
+      } else {
+        resolve();
+      }
+    });
+  }
+
+  /**
+   * Build the command line to call the autogrep
+   */
+  private buildCommandLine(): string | undefined {
+    let commandline = 'cmd.exe /C autogrep.bat ';
+    let fileList: string | undefined;
+    if (this.sources) {
+      this.sources.forEach(source => {
+        if (source.match(/.*\.(CPY|CPB)/gi)) {
+          if (fileList) {
+            fileList = fileList + " " + source;
+          } else {
+            fileList = source;
+          }
+        }
+      });
+    }
+    if (fileList) {
+      commandline = commandline + fileList;
+      return commandline;
+    }
+    return undefined;
+  }
+
+  /**
+   * Sort the result according the source name. The similar sources names comes first
+   */
+  private sortResultsAccordingSourceName(cblSources: string[]): string[] {
+    return cblSources;
+  }
+
+  /**
+   * Add source to find
+   * 
+   * @param source 
+   */
+  public add(source: string) {
+    let path = new Path(source);
+    path = new Path(path.fullPathWin());
+    let s = path.baseName() + path.extension();
+    if (this.sources) {
+      this.sources.concat(s);
+    } else {
+      this.sources = [s];
+    }
+  }
+  
+}
--- src/preproc/preproc.ts
+++ src/preproc/preproc.ts
@@ -1,5 +1,6 @@
 'use babel';
 import { Executor, Path, Process, GenericExecutor } from 'rech-editor-vscode';
+import { Autogrep } from '../autogrep/autogrep';
 
 export class Preproc implements GenericExecutor {
 
@@ -19,9 +20,9 @@ export class Preproc implements GenericExecutor {
    */
   public setPath(path: string | Path): Preproc {
     if (path instanceof Path) {
-      this.path = path.fullPath();
+      this.path = path.fullPathWin();
     } else {
-      this.path = path;
+      this.path = new Path(path).fullPathWin();
     }
     return this;
   }
@@ -32,11 +33,44 @@ export class Preproc implements GenericExecutor {
   }
 
   /**
-   * Run preprocess
+   * Exec the preprocess
    */
   public exec(file: string) {
+    return new Promise((resolve, reject) => {
+      if (file.match(/.*\.(CPY|CPB)/)) {
+        new Autogrep([new Path(this.path).fileName()]).find().then((cblFiles) => {
+          let cblFile = cblFiles[0];
+          this.setPath(new Path(this.path).directory() + cblFile);
+          if (cblFile) {
+            this.execPreproc(file).then((process) => {
+              resolve(process);
+            }).catch(() => {
+              reject();
+            });
+          } else {
+            reject();
+          }
+        }).catch(() => {
+          reject();
+        });
+      } else {
+        this.execPreproc(file).then((process) => {
+          resolve(process);
+        }).catch(() => {
+          reject();
+        });
+      }
+    });
+  }
+
+  /**
+   * Run the preproc
+   * 
+   * @param file 
+   */
+  private execPreproc(file: string) {
     return new Promise((resolve) => {
-      let comandline = (this.getCmdArgs(file)).toString().replace(/,/g, " ") + " " + this.path.replace("file:///f%3A", "F:").replace(/\//g, "\\");
+      let comandline = this.buildCommandLine(file);
       new Executor().runAsync(comandline, (process: Process) => {
         resolve(process);
       });
@@ -44,12 +78,45 @@ export class Preproc implements GenericExecutor {
   }
 
   /**
-   * Return the comand line args
+   * Builds the final command line for preproc execution
+   * 
+   * @param file result filename
    */
-  private getCmdArgs(file: string): string[] {
-    const optionsWithFile = Object.assign({}, this.options);
-    optionsWithFile[optionsWithFile.length - 1] = optionsWithFile[optionsWithFile.length - 1] + file;
-    return ['preproc.bat'].concat(optionsWithFile);
+  private buildCommandLine(file: string): string {
+    let finalCmd = "preproc.bat ";
+    finalCmd = finalCmd + this.path + " ";
+    finalCmd = finalCmd + this.injectFileWithinAsParameter(file).join(" ");
+    finalCmd = finalCmd.replace(/,/g, " ");
+    finalCmd = finalCmd + " -is"; // Only isCobol sources
+    finalCmd = finalCmd.replace(/\//g, "\\");
+    return finalCmd;
   }
+
+  /**
+   * Injects the filename within the 'as' command line parameter
+   * 
+   * @param file result filename
+   */
+  private injectFileWithinAsParameter(file: string): string[] {
+    let optionsWithFile = this.cloneOptions();
+    // Updates the 'as' parameter adding the result filename.
+    // This parameter is always the last one
+    let asParameter = optionsWithFile[this.options.length - 1];
+    asParameter = asParameter + file;
+    optionsWithFile[this.options.length - 1] = asParameter;
+    //
+    return optionsWithFile;
+  }
+
+  /**
+   * Clones the original options
+   */
+  private cloneOptions(): string[] {
+    let cloned: string[] = [];
+    this.options.forEach((x) => {
+      cloned.push(x);
+    });
+    return cloned;
+  }  
 
 }
