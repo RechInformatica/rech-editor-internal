b4de9d1dbd1e172da2ea3ad5fe7a10773f58dae8
Gustavo Cassel <guscassel@gmail.com>
2018-12-20 15:24:52 -0200

    Improved comment puller to identify COPY files and extract the description from their headers

--- src/comment/CommentPuller.ts
+++ src/comment/CommentPuller.ts
@@ -12,15 +12,93 @@ export class CommentPuller {
         let editor = new Editor();
         let cursor = editor.getCursors()[0];
         let buffer = editor.getEditorBuffer();
-        let bufferLines = buffer.split("\n");
-        let file = editor.getCurrentFileName();
+        let bufferLines = buffer.split("\r\n");
+        let currentFileName = editor.getCurrentFileName();
+        let currentLineText = bufferLines[cursor.line];
+        switch(true) {
+            case this.isCopyDeclaration(currentLineText): {
+                this.handleCopyComment(currentLineText, editor.getCurrentFileDirectory(), cursor, bufferLines);
+                break;
+            }
+            default: {
+                this.handleDeclarationComment(bufferLines, cursor, buffer, currentFileName);
+                break;
+            }
+        }
+    }
+
+    /**
+     * Returns true if the current line represents a copy declaration
+     *
+     * @param lineText current line text
+     */
+    private isCopyDeclaration(lineText: string): boolean {
+        if (/\s+(COPY|copy).*/.exec(lineText)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Handles the comment exraction from copy files
+     *
+     * @param lineText current line text
+     * @param currentFileDirectory current file directory
+     * @param cursor cursor position
+     * @param bufferLines buffer lines
+     */
+    private handleCopyComment(lineText: string, currentFileDirectory: string, cursor: RechPosition, bufferLines: string[]): void {
+        let copyFileName = this.extractCopyNameFromCurrentLine(lineText);
+        if (copyFileName !== "") {
+            let comment = this.extractCommentFromCopy(currentFileDirectory + copyFileName);
+            this.handleCommentPulling(comment, cursor, bufferLines);
+        }
+    }
+
+    /**
+     * Extracts the copy filename from current line
+     *
+     * @param currentLineText current line text
+     */
+    private extractCopyNameFromCurrentLine(currentLineText: string): string {
+        let splittedLine = currentLineText.trim().split(/[ |.]+/);
+        if (splittedLine.length > 3 && splittedLine[0].toUpperCase() === "COPY") {
+            return splittedLine[1] + "." + splittedLine[2];
+        }
+        return "";
+    }
+
+    /**
+     * Extracts the comment from copy header
+     *
+     * @param copyFileName copy filename
+     */
+    private extractCommentFromCopy(copyFileName: string): string {
+        let copyBuffer = new File(copyFileName).loadBufferSync("latin1").split("\n");
+        let comment = "";
+        if (copyBuffer.length > 0) {
+            comment = copyBuffer[1];
+        }
+        return new CobolDocParser().parseSingleLineCobolDoc(comment).comment;
+    }
+
+    /**
+     * Handles the comment exraction from element declaration
+     *
+     * @param bufferLines buffer lines
+     * @param cursor cursor position
+     * @param buffer buffer
+     * @param currentFileName current file name
+     */
+    private handleDeclarationComment(bufferLines: string[], cursor: RechPosition, buffer: string, currentFileName: string): void {
         let word = new CobolWordFinder().findWordAt(bufferLines[cursor.line], cursor.column);
         new CobolDeclarationFinder(buffer)
-            .findDeclaration(word, file, (cacheFileName: string) => {
-                return this.fireSourceExpander(word, file, cacheFileName);
+            .findDeclaration(word, currentFileName, (cacheFileName: string) => {
+                return this.fireSourceExpander(word, currentFileName, cacheFileName);
             })
             .then((position: RechPosition) => {
-                this.handleCommentPulling(cursor, position, bufferLines);
+                let comment = this.extractCommentFromDefinition(position, bufferLines);
+                this.handleCommentPulling(comment, cursor, bufferLines);
             })
             .catch(() => {
                 new Editor().showWarningMessage("Declaração de '" + word + "' não encontrada. Não é possível buscar o comentário.");
@@ -47,8 +125,7 @@ export class CommentPuller {
      * @param position position from where comment will be pulled
      * @param bufferLines buffer lines
      */
-    private handleCommentPulling(cursor: RechPosition, position: RechPosition, bufferLines: string[]): void {
-        let comment = this.extractCommentFromDefinition(position, bufferLines);
+    private handleCommentPulling(comment: string, cursor: RechPosition, bufferLines: string[]): void {
         if (comment.trim().length == 0) {
             new Editor().showWarningMessage("Sem comentário vinculado.");
         } else {
