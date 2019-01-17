effde639705851d471e2cc05fe23bfc3971f60cd
Vanduir <vanduir@rech.com.br>
2019-01-17 11:35:52 -0200

    Adjusted openWFPF routine to consider WorkingCopy

--- src/open/OpenWFPF.ts
+++ src/open/OpenWFPF.ts
@@ -1,4 +1,5 @@
-import { Editor, File, Scan} from "rech-editor-cobol";
+import { Editor, File, Scan, Path} from "rech-editor-cobol";
+import { WorkingCopy } from "../wc/WorkingCopy";
 
 /**
  * Class for opening wf/pf files
@@ -18,14 +19,13 @@ export class OpenWFPF {
     /**
      * Opens the WFPF files from the CBL file or opens the CBL file from the WF or PF file
      */
-    public open() {
+    public async open() {
         /** RegEx to find WFPF files */
         const regexWFPF = /(.*)(WF|PF).CPY/gi;
         /** RegEx to find .CBL files */
         const regexCBL = /(.*).CBL/gi;
         /** RegEx to find the constant from the function where the cursor is positioned */
         const regexFunction = /\s([A-Z]+\-FUN\-[A-Z]+)/gi;
-
         /** Get the current file name */
         let filename = this.editor.getCurrentFileName();
         /** If the current file is a WF | PF file */
@@ -33,6 +33,7 @@ export class OpenWFPF {
         if (result) {
             /** Name of the Cobol extension file */
             let cobolFile = result[1] + ".CBL";
+            cobolFile = await this.resolvePath(cobolFile);
             this.editor.openFile(cobolFile, () => {
                 /** Name of the function */
                 let resultFunction = regexFunction.exec(this.editor.getCurrentLine());
@@ -53,9 +54,26 @@ export class OpenWFPF {
         } else {
             result = regexCBL.exec(filename);
             if (result) {
-                this.editor.openFile(result[1] + "WF.CPY");
-                this.editor.openFile(result[1] + "PF.CPY");
+                let wfFileName = await this.resolvePath(result[1] + "WF.CPY");
+                let pfFileName = await this.resolvePath(result[1] + "PF.CPY");
+                this.editor.openFile(wfFileName);
+                this.editor.openFile(pfFileName);
             }
         }
+    }
+
+    /**
+     * Resolve the fileName path
+     *
+     * @param fileName
+     */
+    private async resolvePath(fileName: string): Promise<string> {
+        let wc = await WorkingCopy.current();
+        let path = new Path(fileName);
+        let result = wc.getSourcesDir() + path.fileName();
+        if (!new File(result).exists()) {
+            result = "F:\\Fontes\\" + path.fileName();
+        }
+        return result;
     }
 }
