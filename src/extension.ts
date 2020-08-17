c0d541778c2b5402ff51af182a2abbc5ee89c3c4
Vanduir <vanduir@rech.com.br>
2020-08-17 11:04:58 -0300

    Add JRISocketUtils at the openThis function

--- src/extension.ts
+++ src/extension.ts
@@ -90,10 +90,12 @@ export function activate(_context: any) {
         fileOpener.addPathForFileSearching("C:\\FONTES\\java\\des\\JRIWebServiceApi\\src\\main\\java\\");
         fileOpener.addPathForFileSearching("C:\\FONTES\\java\\des\\JRIRechApplicationServer\\src\\main\\java\\");
         fileOpener.addPathForFileSearching("C:\\FONTES\\java\\des\\JRIUtil\\src\\main\\java\\");
+        fileOpener.addPathForFileSearching("C:\\FONTES\\java\\des\\JRISocketUtils\\src\\main\\java\\");
         fileOpener.addPathForFileSearching("F:\\FONTES\\java\\JRISiger\\src\\main\\java\\");
         fileOpener.addPathForFileSearching("F:\\FONTES\\java\\JRIWebServiceApi\\src\\main\\java\\");
         fileOpener.addPathForFileSearching("F:\\FONTES\\java\\JRIRechApplicationServer\\src\\main\\java\\");
         fileOpener.addPathForFileSearching("F:\\FONTES\\java\\JRIUtil\\src\\main\\java\\");
+        fileOpener.addPathForFileSearching("F:\\FONTES\\java\\JRISocketUtils\\src\\main\\java\\");
         fileOpener.openFromCurrentLine();
     }));
     context.subscriptions.push(commands.registerCommand('rech.editor.internal.OpenDebugSource', () => {
